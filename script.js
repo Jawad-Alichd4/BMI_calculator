b94dc6f5bf81c3d1150f477960a5d14a13fe625a
Jawad-Alichd4 <jawadalichd4@gmail.com>
2024-10-11 00:57:28 -0700

    add some  functionality to js

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 function calculateBMI() {
 var inputweight = document.getElementById("inputweight");
-var input-height = document.getElementById("inputheight");
-if(inputweight === "" && inputheight === "")
+var inputheight = document.getElementById("inputheight");
+var weightvalue = inputweight.value;
+var heightvalue = inputheight.value;
+console.log(weightvalue)
+if(weightvalue === "" && heightvalue === "")
 {
     alert("input some values");
 }
+ else if(weightvalue < 0 || heightvalue < 0)
+ {
+     alert("value must be positive")
+ }
+ var squareheight = heightvalue * heightvalue;
+ var BMIOutput = weightvalue / squareheight;
+ 
+ var output = (document.getElementById("output"));
+ output.innerHTML  = BMIOutput;
+ if(output < 18.5) {
+    output.value += "underweight";
+ }
+ else if(output < 24.9 && output >= 18.5) {
+     output.value += "  normal "
+ }
+ else if(output < 29 && output >= 24.9) {
+    output.value += "  overwieght"
+ }
 
+ 
 }
 
 

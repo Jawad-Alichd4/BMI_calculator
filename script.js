a9658e6d57a847f8e7ea4420c117c6189a1a6206
Jawad-Alichd4 <jawadalichd4@gmail.com>
2024-10-11 00:29:54 -0700

    enter basic js

---
+++ script.js
@@ -0,0 +1,11 @@
+function calculateBMI() {
+var inputweight = document.getElementById("inputweight");
+var input-height = document.getElementById("inputheight");
+if(inputweight === "" && inputheight === "")
+{
+    alert("input some values");
+}
+
+}
+
+

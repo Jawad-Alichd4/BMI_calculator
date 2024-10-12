d990c9146e5be440c0c163c8fff47d4c65a5efc2
Jawad-Alichd4 <jawadalichd4@gmail.com>
2024-10-11 22:10:40 -0700

    modified js

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ console.log(weightvalue)
 if(weightvalue === "" && heightvalue === "")
 {
     alert("input some values");
+    return;
 }
  else if(weightvalue < 0 || heightvalue < 0)
  {
-     alert("value must be positive")
+     alert("value must be positive");
+     return;
  }
  var squareheight = heightvalue * heightvalue;
  var BMIOutput = weightvalue / squareheight;
@@ -20,16 +22,16 @@ if(weightvalue === "" && heightvalue === "")
   
 
  if(BMIOutput < 18.5) {
-    output.value = "underweight" + BMIOutput;
+    output.innerHTML = "underweight :" + BMIOutput;
  }
- else if(output < 24.9 && output >= 18.5) {
-     output.value = "  normal " + output;
+ else if(BMIOutput < 24.9 && output >= 18.5) {
+     output.innerHTML = "  normal : " + output;
  }
- else if(output < 29 && output >= 24.9) {
-    output.value = "overwieght" + output;
+ else if(BMIOutput < 29 && output >= 24.9) {
+    output.innerHTML = "overwieght :" + output;
  }
-  else if(output > 29 ) {
-     output.value = "Obesity" + output;
+  else if(BMIOutput > 29 ) {
+     output.innerHTML = "Obesity :" + output;
   }
  
 }
